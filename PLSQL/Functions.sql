2b85b0c196d1c577a02792d62b5d59f89494cc71
Rishika S <126490635+10rishika06@users.noreply.github.com>
2026-02-27 23:25:36 +0530

    Added Functions.sql

--- PLSQL/Functions.sql
+++ PLSQL/Functions.sql
@@ -1,1 +1,16 @@
+-- Function: Get High Claim Members
 
+CREATE OR REPLACE FUNCTION get_high_claim_members(p_limit NUMERIC)
+RETURNS TABLE(member_name VARCHAR, total_claim NUMERIC) AS
+$$
+BEGIN
+    RETURN QUERY
+    SELECT m.member_name,
+           SUM(c.claim_amount)
+    FROM members m
+    JOIN claims c
+    ON m.member_id = c.member_id
+    GROUP BY m.member_name
+    HAVING SUM(c.claim_amount) > p_limit;
+END;
+$$ LANGUAGE plpgsql;
