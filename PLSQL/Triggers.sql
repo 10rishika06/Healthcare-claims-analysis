7df846496db95bd4b3ac26e3abfb9accebd32a5c
Rishika S <126490635+10rishika06@users.noreply.github.com>
2026-02-27 23:29:21 +0530

    Added Triggers.sql

--- PLSQL/Triggers.sql
+++ PLSQL/Triggers.sql
@@ -1,1 +1,20 @@
+-- Trigger Function: Validate Claim Amount
 
+CREATE OR REPLACE FUNCTION validate_claim_amount()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    IF NEW.claim_amount < 0 THEN
+        RAISE EXCEPTION 'Claim amount cannot be negative';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--Call Statement
+CREATE TRIGGER check_claim_amount
+BEFORE INSERT OR UPDATE
+ON claims
+FOR EACH ROW
+EXECUTE FUNCTION validate_claim_amount();
